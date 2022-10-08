75c7f7ecfeaa63be25bf2a0504400b4b760ad8a2
정성현 <113809406+skqls@users.noreply.github.com>
2022-10-08 11:11:19 +0900

    DFS와BFS 32468kb 100ms

---
+++ 1260/1260_csh.py
@@ -0,0 +1,49 @@
+
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+
+def main():
+    def dfs(graph, v, visited):
+        visited[v] = True
+        print(v, end=' ')
+
+        for i in graph[v]:
+            if not visited[i]:
+                dfs(graph, i, visited)
+
+
+    def bfs(graph, v, visited):
+        queue = deque([v])
+        visited[v] = True
+
+        while queue:
+            now = queue.popleft()
+            print(now, end=' ')
+
+            for i in graph[now]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+
+
+
+    n, m, v = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    for i in range(1, n + 1):
+        graph[i].sort()
+
+    visited = [False] * (n + 1)
+    dfs(graph, v, visited)
+    print()
+    visited = [False] * (n + 1)
+    bfs(graph, v, visited)
+
+if __name__ == '__main__':
+    main()
