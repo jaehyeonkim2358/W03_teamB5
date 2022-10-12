ba4376df1cd7c10939ef09278bf23c12dd6e9b5d
wnhlee <2wheeh@gmail.com>
2022-10-12 19:10:05 +0900

    탈출 주석 수정

--- 3055/3055_lwh.py
+++ 3055/3055_lwh.py
@@ -10,15 +10,13 @@ input= sys.stdin.readline
 # 고슴도치 물 통과 X, 물 인접 통과 X
 # 물 비버굴 통과 X
 
-# 굴까지 가는 경로에 고슴도치가 물보다 늦게 도착하는 지점이 있으면 안됨 인접하면 안됨 
+# 굴까지 가는 경로에 고슴도치가 물보다 늦게 혹은 동시에 도착하는 지점이 있으면 안됨 
 # 물 도착시간 먼저 매핑 후 고슴도치가 물을 피해서 굴까지 가는 최단 경로 찾기
 # v.time > u.time + 1 일때 v로 이동
-# 굴은 물갈땐 0 으로 막아두고 INF 초기화 . 고슴도치 스캔 후 결과 출력 그대로 0이면 안간 것
+# 굴은 물갈땐 0 으로 막아두고 INF 초기화 . 고슴도치 스캔 후 결과 출력 그대로 INF이면 안간 것
 
 
-def run(forest, s, R, C):
-    q = deque()
-    q.append(s)
+def run(forest, q, R, C):
 
     dx = [-1, 1, 0 ,0]
     dy = [0, 0, -1, 1]
@@ -46,7 +44,8 @@ def init():
     
     INF = 2**31
 
-    water = []
+    water = deque([])
+    kak = deque([])
     for x in range(R):
         row = list(input().rstrip())
         for y in range(C):
@@ -59,7 +58,8 @@ def init():
                 forest[x][y] = 0
             elif obj == 'S':
                 forest[x][y] = INF
-                kak = (x,y)
+                kak.append((x, y))
+                # kak = (x,y)
             elif obj == '*':
                 forest[x][y] = 0
                 water.append((x, y))
@@ -67,11 +67,10 @@ def init():
                 forest[x][y] = INF
     
     if water:
-        for s in water:
-            run(forest, s, R, C)
+        run(forest, water, R, C)
 
     forest[cave[0]][cave[1]] = INF
-    forest[kak[0]][kak[1]] = 0
+    forest[kak[0][0]][kak[0][1]] = 0
 
     run(forest, kak, R, C)
 
